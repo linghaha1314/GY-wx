50fa8fa2eebf8307828b7c1eda832454b637902c
linghaha1314 <879073224@qq.com>
2020-10-10 17:40:41 +0800

    引入ui框架

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import { Button,Icon } from 'vant';
 
 Vue.config.productionTip = false
 
+Vue.use(Button)
+Vue.use(Icon)
+
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
