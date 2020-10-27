14362b91ce18971caf0e7d76a4cedf954346a5af
linghaha1314 <879073224@qq.com>
2020-10-27 10:37:51 +0800

    替换图片

---
+++ src/views/activities-add.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <van-nav-bar :title="name" left-arrow>
+      <template #left>
+        <span style="color: #17d4b5" @click="$router.back(-1)">返回</span>
+      </template>
+    </van-nav-bar>
+  </div>
+</template>
+<script>
+    export default {
+        name: 'test',
+        data(){
+            return{
+                value: '',
+            }
+        },
+        computed:{
+            name(){
+                return this.$route.name
+            }
+        }
+    }
+</script>
